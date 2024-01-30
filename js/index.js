c2ca1e188ac957416851c06f2887d941615a9844
sameermakwana19 <sameermakwana19@gmail.com>
2024-01-30 19:05:48 +0530

    second commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,27 @@
 const collapsible = document.querySelectorAll('.collapsible');
+// console.log(collapsible);
 
 collapsible.forEach((item)=>{
-  item.addEventListener('click',()=>{
+  const header = item.querySelector('.collapsible__header');
+  const icon = header.getElementsByTagName('i')[0];
+  header.addEventListener('click',()=>{
     item.classList.toggle('collapsible--expanded');
+    icon.classList.toggle('fa-xmark');
   })
-})
+});
+
+const side_overlay_btn = document.querySelector('.side-overlay-btn');
+const side_overlay = document.querySelector('.side-overlay');
+
+side_overlay_btn.addEventListener('click',function(){
+  side_overlay.classList.toggle('side-overlay--expanded');
+});
+// console.log(side_overlay_btn);
+
+const side_overlay_close_btn = document.querySelector('.cross-icon');
+
+side_overlay_close_btn.addEventListener('click',function(){
+  side_overlay.classList.toggle('side-overlay--expanded');
+});
+
+
